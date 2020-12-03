36e5834208b4dc656482513d4054c44a03b5dd57
uaryal <73482648+uaryal@users.noreply.github.com>
2020-12-02 22:00:38 -0500

    Add files via upload

---
+++ udayfitnesstracker.py
@@ -0,0 +1,103 @@
+class FitnessTracker:
+    
+    def __init__():
+        self.age
+        self.gender
+        self.estimation_formula
+        self.goal
+        self.regimes
+
+    def CalorieCalculator():
+        """
+        Definition:
+        Args:
+        Returns:
+        """
+
+    def HowOften():
+     #Gain, maintain, or lose weight (goal)
+        if self.gender == "M" and self.age in range (18,100) and self.goal == "Maintain"
+            if weight in range (120,150):
+                self.regimes = 
+            elif weight in range (151, 170):
+                self.regimes = 
+            elif weight in range (171, 220):
+                self.regimes = 
+            elif weight in range (221, 250):
+                self.regimes = 
+        
+        if self.gender == "F" and self.age in range (18,100) and self.goal == "Maintain"
+            if weight in range (80, 105):
+                self.regimes = 
+            elif weight in range (106, 125):
+                self.regimes = 
+            elif weight in range (126, 145):
+                self.regimes = 
+            elif weight in range (146, 165):
+                self.regimes = 
+        
+        if self.gender == "M" and self.age in range (18,100) and self.goal == "Gain"
+            if weight in range (120, 150):
+                self.regimes = 
+            elif weight in range (151, 170):
+                self.regimes = 
+            elif weight in range (171, 220):
+                self.regimes = 
+            elif weight in range (221, 250):
+                self.regimes = 
+            
+        if self.gender == "F" and self.age in range (18,100) and self.goal == "Gain"
+            if weight in range (80, 105):
+                self.regimes = 
+            elif weight in range (106, 125):
+                self.regimes = 
+            elif weight in range (126, 145):
+                self.regimes = 
+            elif weight in range (146, 165):
+                self.regimes = 
+
+        if self.gender == "M" and self.age in range (18,100) and self.goal == "Lose"
+            if weight in range (140, 200):
+                self.regimes = 
+            elif weight in range (201, 300):
+                self.regimes = 
+            elif weight in range (301, 400):
+                self.regimes = 
+            elif weight > 401:
+                self.regimes = 
+        
+        if self.gender == "F" and self.age in range (18,100) and self.goal == "Lose"
+            if weight in range (80, 105):
+                self.regimes = 
+            elif weight in range (106, 125):
+                self.regimes = 
+            elif weight in range (126, 399):
+                self.regimes = 
+            elif weight > 400:
+                self.regimes = 
+
+        """
+        Definition:
+        Args:
+        Returns:
+        """    
+    
+    def WorkoutRegime():
+        #Three regimes depending on gain, maintain or lose
+        #Different interval based on three regimes
+        
+    def Diet():
+        #Macro ratios based on gaining, maintaining, or losing weight, along with the calories
+        #Ratio of calories you want to be protein, carbs, and fat
+        #Text file of diets
+        
+    def Cardio():
+        #Amount of cardio based on ratio
+        #One cardio workout per parameter
+        
+    def Cooldown():
+        #Three cooldown routines
+    
+    def Tips():
+        #Based on three options
+        
